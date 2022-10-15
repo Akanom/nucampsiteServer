847e1649af934d938504eac194d4671290c53fc4
Oluwajuwon Akanbi <omokoreakanbi@gmail.com>
2022-10-15 14:09:44 +0200

    Authentication

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const promotionRouter = require("./routes/promotionRouter");
 const partnerRouter = require("./routes/promotionRouter");
 
 const mongoose = require("mongoose");
+const { start } = require("repl");
 //Set up default mongoose connection
 const url = "mongodb://localhost:27017/nucampsite";
 const connect = mongoose.connect(url, {
@@ -20,8 +21,10 @@ const connect = mongoose.connect(url, {
   useUnifiedTopology: true,
 });
 //Get the default connection
-connect.then(() => console.log("Connected correctly to the server"),
-err=>console.log(err));
+connect.then(
+  () => console.log("Connected correctly to the server"),
+  (err) => console.log(err)
+);
 
 var app = express();
 
@@ -33,6 +36,35 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// Authentication
+function auth(req, res, next) {
+  console.log(req.headers);
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    const err = new Error("You are not authenticated!");
+    res.setHeader("WWW-Authenticate", "Basic");
+    err.status = 401;
+    return next(err);
+  }
+
+  const auth = Buffer.from(authHeader.split(' ')[1], "base64")
+    .toString()
+    .split(":");
+  const user = auth[0];
+  const pass = auth[1];
+  if (user === "admin" && pass === "password") {
+    return next(); // authorized
+  } else {
+    const err = new Error("You are not authenticated!");
+    res.setHeader("WWW-Authenticate", "Basic");
+    err.status = 401;
+    return next(err);
+  }
+}
+
+app.use(auth);
+
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
